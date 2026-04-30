38d608a1e96f5b0cc99893d01bcb4516c4559a9a
Shasmit01 <shasmit01@github.com>
2026-04-30 23:36:51 +0530

    Add Replit configuration files

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }:
+{
+  deps = [
+    pkgs.python310Full
+    pkgs.python310Packages.pip
+  ];
+  env = {
+    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    LANG = "en_US.UTF-8";
+  };
+}
